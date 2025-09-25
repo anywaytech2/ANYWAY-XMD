0d711a6e7178c6d79c6dc23a615067470c95c81e
mr Anyway <simonstephano328@gmail.com>
2025-09-25 13:53:19 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -56,7 +56,7 @@ async (conn, mek, m, { from }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363382023564830@newsletter',
+                    newsletterJid: '120363304536947506@newsletter',
                     newsletterName: '𝘼𝙉𝙔𝙒𝘼𝙔-𝙓𝙈𝘿',
                     serverMessageId: 143
                 }
